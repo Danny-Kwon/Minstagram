1ed1041a6473df55d69657f7be34507d78cccd07
Minhyeok <danny9kwon@gmail.com>
2022-11-11 00:26:16 +0900

    Minstagram Firebase Google Login 추가

--- app/src/main/java/com/example/minstagram/LoginActivity.kt
+++ app/src/main/java/com/example/minstagram/LoginActivity.kt
@@ -5,11 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.minstagram.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
     var auth: FirebaseAuth? = null
+    var googleSignInClient: GoogleSignInClient? = null
+    private var GOOGLE_LOGIN_CODE = 9001
     private val binding by lazy{
         ActivityLoginBinding.inflate(layoutInflater)
     }
@@ -18,12 +26,50 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
         with(binding){
             emailSignUpButton.setOnClickListener {
                 signUpInEmail(emailEdittext.text.toString().trim(), passwordEdittext.text.toString().trim())
             }
             emailLoginButton.setOnClickListener {
                 signInEmail(emailEdittext.text.toString().trim(), passwordEdittext.text.toString().trim())
+            }
+            googleLoginButton.setOnClickListener {
+                googleLogin()
+            }
+        }
+    }
+
+    private fun googleLogin() {
+        val signInClient = googleSignInClient?.signInIntent
+        startActivityForResult(signInClient, GOOGLE_LOGIN_CODE)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE){
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result!!.isSuccess){
+                val account = result.signInAccount
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        val credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        auth?.signInWithCredential(credential)?.addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                moveToNext(task.result?.user)
+            } else {
+                Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
             }
         }
     }
